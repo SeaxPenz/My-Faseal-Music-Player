64d9d342be4a7711f5b250280a76b014a7683874
Ajetomobi Abraham <abrahamajetomobi@gmail.com>
2025-04-01 04:15:10 +0100

    Removed builds config and engines config to use node 22

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+//import * as serviceWorkerRegistration from './serviceWorkerRegistration'; //Comment out the line, since there are errors.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+<App />
+</React.StrictMode>
 );
